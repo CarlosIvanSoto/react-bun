4ce6f8839015f572cd84e60110005d44a835b8c5
Carlos Ivan Soto <gameue@hotmail.com>
2022-11-16 17:13:37 -0600

    Component Extend Styles - Ready

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -1,27 +1,23 @@
 import { ReactElement } from "react";
+import { Props as ProductButtonsProps} from "../components/ProductButtons";
+import { Props as ProductCardProps} from "../components/ProductCard";
+import { Props as ProductImagesProps} from "../components/ProductImages";
+import { Props as ProductTtieleProps} from "../components/ProductTitle";
 
-export interface ProductCardProps {
-  product: Product;
-  children?: ReactElement | ReactElement[];
-}
 export interface Product {
-  id: string;
-  title: string;
-  img?: string;
+  id: string
+  title: string
+  img?: string
 }
 export interface ProductContextProps {
-  counter: number;
-  increseBy: (value: number) => void;
-  product: Product;
-}
-export interface ProductButtonsProps {
-  counter: number;
-  increseBy: (value: number) => void;
+  counter: number
+  increseBy: (value: number) => void
+  product: Product
 }
 export interface ProductCardHOCProps {
-  ({ product, children }: ProductCardProps): JSX.Element
-  Title: ({title}: {title?: string}) => JSX.Element
-  Image: ({img}: {img?: string}) => JSX.Element
-  Buttons: () => JSX.Element
+  (Props: ProductCardProps ): JSX.Element
+  Title: (Props: ProductTtieleProps) => JSX.Element
+  Image: (Props: ProductImagesProps) => JSX.Element
+  Buttons: (Props: ProductButtonsProps) => JSX.Element
 }
 
