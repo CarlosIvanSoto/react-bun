d2cfbba81c9dabd48bb9c02894753133be612987
Carlos Ivan Soto <gameue@hotmail.com>
2022-11-16 17:26:33 -0600

    Component Extended Styles - Ready

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,12 +1,17 @@
-import React, { useContext } from "react"
+import React, { CSSProperties, useContext } from "react"
 import { ProductContext } from "./ProductCard"
 
 import styles from '../styles/styles.module.css'
 
-export const ProductButtons = () => {
+export interface Props {
+  className?: string
+  style?: CSSProperties
+}
+
+export const ProductButtons = ({className, style}: Props) => {
     const {increseBy, counter} = useContext(ProductContext)
     return (
-      <div className={styles.buttonsContainer}>
+      <div style={style} className={`${styles.buttonsContainer} ${className}`}>
         <button className={styles.buttonMinus} onClick={() => increseBy(-1)}>-</button>
         <div className={styles.countLabel}>{counter}</div>
         <button className={styles.buttonAdd} onClick={() => increseBy(1)}>+</button>
--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,18 +1,25 @@
-import React, { createContext } from 'react'
+import React, { createContext, CSSProperties, ReactElement } from 'react'
 import styles from '../styles/styles.module.css'
 import { useProduct } from '../hooks/useProduct'
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces'
+import { ProductContextProps, Product } from '../interfaces/interfaces'
 
 export const ProductContext = createContext({} as ProductContextProps)
 
-export const ProductCard = ({product, children}: ProductCardProps) => {
+export interface Props {
+  product: Product
+  children?: ReactElement | ReactElement[]
+  className?: string
+  style?: CSSProperties
+}
+
+export const ProductCard = ({product, children, className, style}: Props) => {
 
   const { counter, increseBy } = useProduct()
   const { Provider } = ProductContext
 
   return (
     <Provider value={{counter, increseBy, product}}>
-      <div className={styles.productCard}>
+      <div className={`${styles.productCard} ${className}`} style={style}>
         { children }
       </div>
     </Provider>
