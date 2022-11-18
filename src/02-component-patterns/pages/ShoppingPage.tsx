93937a56ef4e57d6cadbd94ffbc03b4fe277ffbe
Carlos Ivan Soto <gameue@hotmail.com>
2022-11-18 13:19:57 -0600

    style modificados

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -15,20 +15,13 @@ export const ShoppingPage = () => {
         <ProductCard
           initialValues={{count:4, maxCount:10}}
           key={product.id}
-          product={product} 
-          className='text-white bg-dark'>
+          product={product} >
           {
             (args) => (
               <>
-              <ProductTitle className='text-bold'/>
-              <ProductImage className='custom-image' />
-              <ProductTitle className='text-bold'/>
-              <ProductButtons className='custom-buttons'/>
-              <button onClick={args.reset}>Reset</button>
-              <button onClick={() => args.increaseBy(-2)}>-2</button>
-              <button disabled={args.isMaxCountReachd} onClick={() => args.increaseBy(2)}>+2</button>
-              <span >{args.count}</span>
-              {/* { JSON.stringify(args, null, 3)} */}
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons />
               </>
             )
           }
